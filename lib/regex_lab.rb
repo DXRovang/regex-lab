b00e8262b95bfb8639625f67e11d0bc39021a5ec
Alexander Rovang <wolf_vii@yahoo.com>
2020-12-19 12:54:53 -0500

    commit tree 588c39537e3d4ea126abeed94f4e33a9f05cbbf2

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,22 @@
+require "pry"
 def starts_with_a_vowel?(word)
-
+  word.match?(/^[aeiou]\w+/i)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  search = text.split
+  search.grep(/^\w{5}$/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match?(/[A-Z]\w+[.?!,]/)
 end
 
 def valid_phone_number?(phone)
-
+  #interesting note:  regexp brackets don't like 
+  phone.match?(/\(?(\d{3})[\s\).-]?(\d{3})[\s.-]?(\d{4})/)
 end
